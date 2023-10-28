206382cb32cc3af55ca4248c0f37a2d3ef43ca68
burrito77 <beryl.britto@gmail.com>
2023-10-28 13:07:13 -0500

    workingClient-Server

--- client.c
+++ client.c
@@ -1,121 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 
-// Function to tokenize a text file into words using proper delimiters
-// ...
+int main(int argc, char *argv[]) {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[1024];
+    
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-#define MAX_WORD_LEN 50 // Maximum word length
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("10.100.240.204");  // Replace with the server IP
+    server_addr.sin_port = htons(8080);  // Use the same port as in server
 
-void tokenizeFile(const char* filename) {
-    FILE* file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Error opening input file");
-        return;
+    connect(client_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
+
+    // Send handshake (encryption or decryption)
+    printf("Choose service (encryption/decryption): ");
+    scanf("%s", buffer);
+    send(client_socket, buffer, strlen(buffer), 0);
+
+    while(1) {
+        printf("Enter a word (or 'exit' to quit): ");
+        scanf("%s", buffer);
+        send(client_socket, buffer, strlen(buffer), 0);
+        if(strcmp(buffer, "exit") == 0) {
+            break;
+        }
+        recv(client_socket, buffer, sizeof(buffer)-1, 0);
+        printf("Received: %s\n", buffer);
     }
 
-    char word[MAX_WORD_LEN];
-    while (fscanf(file, "%s", word) == 1) {
-        // Process each word
-        // ...
-    }
-
-    fclose(file);
-}
-
-// Function to connect to the server and call remote functions
-// ...
-#define SERVER_IP "127.0.0.1" // Replace with the actual server IP
-#define SERVER_PORT 8080      // Replace with the actual server port
-
-void connectToServerAndCallRemoteFunctions(const char* word) {
-    int clientSocket;
-    struct sockaddr_in serverAddr;
-
-    // Create a socket
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket < 0) {
-        perror("Error opening socket");
-        exit(1);
-    }
-
-    // Configure server address
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(SERVER_PORT);
-
-    // Resolve server IP and connect to the server
-    if (inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr) <= 0) {
-        perror("Invalid server address");
-        close(clientSocket);
-        exit(1);
-    }
-
-    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr) < 0) {
-        perror("Error connecting to server");
-        close(clientSocket);
-        exit(1);
-    }
-
-    // Implement code to call remote functions (e.g., encrypt) on the server
-    // ...
-
-    // Close the client socket
-    close(clientSocket);
-}
-
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <server_host> <server_port> <input_file>\n", argv[0]);
-        exit(1);
-    }
-
-    char* serverHost = argv[1];
-    int serverPort = atoi(argv[2]);
-    char* inputFile = argv[3];
-
-    int clientSocket;
-    struct sockaddr_in serverAddr;
-
-    // Create a socket
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket < 0) {
-        perror("Error opening socket");
-        exit(1);
-    }
-
-    // Configure server address
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(serverPort);
-
-    // Resolve server host and connect to the server
-    if (inet_pton(AF_INET, serverHost, &serverAddr.sin_addr) <= 0) {
-        perror("Invalid server address");
-        close(clientSocket);
-        exit(1);
-    }
-
-    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr) < 0) {
-        perror("Error connecting to server");
-        close(clientSocket);
-        exit(1);
-    }
-
-    // Read and tokenize the input text file
-    // ...
-
-    // For each word, call the remote encrypt() function on the server
-    // ...
-
-    // Write the encrypted word and key to the output text file
-    // ...
-
-    // Close the client socket
-    close(clientSocket);
-
+    close(client_socket);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,82 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
+#include <pthread.h>
 
-// Function to handle encryption
-void* encrypt(void* args) {
-    // Implement encryption logic
-    // ...
-}
+void *handle_client(void *client_socket_ptr);
 
-// Function to handle decryption
-void* decrypt(void* args) {
-    // Implement decryption logic
-    // ...
-}
+int main(int argc, char *argv[]) {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len;
+    
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        exit(1);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(8080);  // Use your desired port
+
+    bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    listen(server_socket, 5);
+
+    while(1) {
+        client_len = sizeof(client_addr);
+        client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &client_len);
+        
+        pthread_t client_thread;
+        pthread_create(&client_thread, NULL, handle_client, &client_socket);
     }
 
-    int port = atoi(argv[1);
-    int serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t clientLen = sizeof(clientAddr);
-
-    // Create a socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket < 0) {
-        perror("Error opening socket");
-        exit(1);
-    }
-
-    // Configure server address
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(port);
-
-    // Bind the socket
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr) < 0) {
-        perror("Error binding socket");
-        close(serverSocket);
-        exit(1);
-    }
-
-    // Start listening for incoming connections
-    if (listen(serverSocket, 5) < 0) {
-        perror("Error listening");
-        close(serverSocket);
-        exit(1);
-    }
-
-    printf("Server listening on port %d...\n", port);
-
-    while (1) {
-        // Accept incoming connections
-        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientLen);
-        if (clientSocket < 0) {
-            perror("Error accepting client connection");
-            continue;
-        }
-
-        // Create a new thread to handle the client
-        pthread_t clientThread;
-        if (pthread_create(&clientThread, NULL, clientHandler, &clientSocket) != 0) {
-            perror("Error creating thread");
-            close(clientSocket);
-            continue;
-        }
-    }
-
-    // Close the server socket (not reached in this code)
-    close(serverSocket);
-
+    close(server_socket);
     return 0;
 }
+
+void *handle_client(void *client_socket_ptr) {
+    int client_socket = *(int *) client_socket_ptr;
+    char buffer[1024];
+    char handshake[1024];
+
+    // Receive the handshake
+    recv(client_socket, handshake, sizeof(handshake)-1, 0);
+    printf("Received handshake: %s\n", handshake);  // Print the received handshake message
+
+    while(1) {
+        memset(buffer, 0, sizeof(buffer));
+        recv(client_socket, buffer, sizeof(buffer)-1, 0);
+        if(strcmp(buffer, "exit") == 0) {
+            break;
+        }
+
+        printf("Received word: %s\n", buffer);  // Print the received word before processing
+
+        // Encrypt or decrypt based on handshake
+        for(int i = 0; i < strlen(buffer); i++) {
+            if(strcmp(handshake, "encryption") == 0) {
+                buffer[i]++;
+            } else if(strcmp(handshake, "decryption") == 0) {
+                buffer[i]--;
+            }
+        }
+
+        printf("Processed word: %s\n", buffer);  // Print the processed word
+
+        send(client_socket, buffer, strlen(buffer), 0);
+    }
+
+    close(client_socket);
+    return NULL;
+}
+
