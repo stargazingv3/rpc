3f7f4dd773180c9d6500b5209b961d375701224c
burrito77 <beryl.britto@gmail.com>
2023-10-29 06:17:22 -0500

    Working RPC mechanisms

--- client.c
+++ client.c
@@ -3,37 +3,122 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 
-int main(int argc, char *argv[]) {
-    int client_socket;
-    struct sockaddr_in server_addr;
+#define SERVER_IP "10.100.240.204"
+#define SERVER_PORT 8080
+
+// Define the RPC protocol operations
+enum RPCOperation {
+    RPC_ENCRYPT,
+    RPC_DECRYPT,
+};
+
+// Structure for an RPC request
+struct RPCRequest {
+    enum RPCOperation operation;
+    char text[1024];
+    int key;
+};
+
+// Function to send an RPC request (client stub)
+void send_rpc_request(int socket_fd, enum RPCOperation operation, const char* text) {
+    struct RPCRequest request;
+    request.operation = operation;
+    strncpy(request.text, text, sizeof(request.text));
+    send(socket_fd, &request, sizeof(request), 0);
+}
+
+// Function to receive an RPC response (client stub)
+char* receive_rpc_response(int socket_fd) {
     char buffer[1024];
-    
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    ssize_t bytes_received = recv(socket_fd, buffer, sizeof(buffer), 0);
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';
+        return strdup(buffer);
+    } else {
+		//printf("Buffer received: %s", strdup(buffer));
+        perror("Error receiving response from server");
+        return NULL;
+    }
+}
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("10.100.240.204");  // Replace with the server IP
-    server_addr.sin_port = htons(8080);  // Use the same port as in server
-
-    connect(client_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
-
-    // Send handshake (encryption or decryption)
-    printf("Choose service (encryption/decryption): ");
-    scanf("%s", buffer);
-    send(client_socket, buffer, strlen(buffer), 0);
-
-    while(1) {
-        printf("Enter a word (or 'exit' to quit): ");
-        scanf("%s", buffer);
-        send(client_socket, buffer, strlen(buffer), 0);
-        if(strcmp(buffer, "exit") == 0) {
-            break;
-        }
-        recv(client_socket, buffer, sizeof(buffer)-1, 0);
-        printf("Received: %s\n", buffer);
+int main(int argc, char* argv[]) {
+    if (argc < 2 || (argc - 1) % 3 != 2) {
+        fprintf(stderr, "Usage: %s <command> <file> [<command> <file>] ...\n", argv[0]);
+		printf("Number of args: %d", argc);
+        exit(EXIT_FAILURE);
     }
 
+    int client_socket;
+    struct sockaddr_in server_addr;
+
+    // Create socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("Invalid server IP address");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to the server
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection to server failed");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    // Send RPC requests for each command and file
+    for (int i = 1; i < argc; i += 3) {
+        enum RPCOperation operation;
+        if (strcmp(argv[i], "encrypt") == 0) {
+            operation = RPC_ENCRYPT;
+        } else if (strcmp(argv[i], "decrypt") == 0) {
+            operation = RPC_DECRYPT;
+        } else {
+            fprintf(stderr, "Invalid command: %s\n", argv[i]);
+            continue;
+        }
+
+        const char* file_name = argv[i + 1];
+
+        // Read text from the specified file
+        FILE* file = fopen(file_name, "r");
+        if (file == NULL) {
+            perror("Error opening file");
+            continue;
+        }
+
+        char text[1024];
+        if (fgets(text, sizeof(text), file) != NULL) {
+            // Remove trailing newline if present
+            size_t len = strlen(text);
+            if (len > 0 && text[len - 1] == '\n') {
+                text[len - 1] = '\0';
+            }
+            send_rpc_request(client_socket, operation, text);
+
+            char* response = receive_rpc_response(client_socket);
+            if (response) {
+                printf("Server Response:\n%s\n", response);
+                free(response);
+            }
+        } else {
+            fprintf(stderr, "Error reading text from file: %s\n", file_name);
+        }
+
+        fclose(file);
+    }
+
+    // Cleanup and exit
     close(client_socket);
     return 0;
 }
--- server.c
+++ server.c
@@ -5,68 +5,161 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
-void *handle_client(void *client_socket_ptr);
+#define PORT 8080
+#define MAX_CLIENTS 10
 
-int main(int argc, char *argv[]) {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len;
-    
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+// Define the RPC protocol operations
+enum RPCOperation {
+    RPC_ENCRYPT,
+    RPC_DECRYPT,
+};
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(8080);  // Use your desired port
+// Structure for an RPC request
+struct RPCRequest {
+    enum RPCOperation operation;
+    char text[1024];
+	int key;
+};
 
-    bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
-    listen(server_socket, 5);
+char* server_encrypt(const char* text) {
+    printf("Encrypting, returning %s\n", text);
 
-    while(1) {
-        client_len = sizeof(client_addr);
-        client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &client_len);
-        
-        pthread_t client_thread;
-        pthread_create(&client_thread, NULL, handle_client, &client_socket);
+    // Check if text is NULL
+    if (text == NULL) {
+        // Handle the case where text is NULL (optional)
+        perror("Input text is NULL");
+        return NULL;
     }
 
-    close(server_socket);
-    return 0;
+    // Attempt to duplicate the text
+    char* copy = strdup(text);
+
+    // Check if memory allocation was successful
+    if (copy == NULL) {
+        // Handle allocation failure
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Successful strdup, attempting return %s\n", text);
+
+    return text;
 }
 
-void *handle_client(void *client_socket_ptr) {
-    int client_socket = *(int *) client_socket_ptr;
-    char buffer[1024];
-    char handshake[1024];
 
-    // Receive the handshake
-    recv(client_socket, handshake, sizeof(handshake)-1, 0);
-    printf("Received handshake: %s\n", handshake);  // Print the received handshake message
-
-    while(1) {
-        memset(buffer, 0, sizeof(buffer));
-        recv(client_socket, buffer, sizeof(buffer)-1, 0);
-        if(strcmp(buffer, "exit") == 0) {
+// Function to decrypt the text (server stub)
+char* server_decrypt(const char* text) {
+    // Implement the server-side decryption logic here
+    // This is just a placeholder
+    return text;
+}
+	
+// Function to handle an RPC request
+char* handle_rpc_request(const struct RPCRequest* request) {
+    char* result = NULL;
+	printf("Request info: %s\n", request->operation);
+    switch (request->operation) {
+        case RPC_ENCRYPT:
+			printf("Attempting Encryption\n");
+            result = server_encrypt(request->text);
+			printf("Encryption finished, received %s\n", result);
             break;
+        case RPC_DECRYPT:
+            result = server_decrypt(request->text);
+            break;
+        default:
+			printf("Given empty RPCRequest\n");
+            // Invalid operation
+            break;
+    }
+    return result;
+}
+
+// Thread function to handle client requests
+void* handle_client(void* client_socket) {
+	
+    int socket_fd = *(int*)client_socket;
+    struct RPCRequest request;
+    char* response;
+	
+    while (recv(socket_fd, &request, sizeof(request), 0) > 0) {
+        response = handle_rpc_request(&request);
+		printf("Handling Client\n");
+		
+        if (response) {
+			//printf(response);
+            send(socket_fd, response, strlen(response), 0);
+            //free(response);
         }
+    }
+	printf("Finished handling client\n");
+    close(socket_fd);
+    //free(client_socket);
+    return NULL;
+}
 
-        printf("Received word: %s\n", buffer);  // Print the received word before processing
+int main() {
+    int server_socket, new_socket;
+    struct sockaddr_in server_addr, new_addr;
+    socklen_t addr_size;
+    pthread_t tid[MAX_CLIENTS];
+    int clients_count = 0;
 
-        // Encrypt or decrypt based on handshake
-        for(int i = 0; i < strlen(buffer); i++) {
-            if(strcmp(handshake, "encryption") == 0) {
-                buffer[i]++;
-            } else if(strcmp(handshake, "decryption") == 0) {
-                buffer[i]--;
-            }
-        }
-
-        printf("Processed word: %s\n", buffer);  // Print the processed word
-
-        send(client_socket, buffer, strlen(buffer), 0);
+    // Create socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("Error creating server socket");
+        exit(EXIT_FAILURE);
     }
 
-    close(client_socket);
-    return NULL;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind socket
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error binding server socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for client connections
+    if (listen(server_socket, 10) == 0) {
+        printf("Server is listening on port %d...\n", PORT);
+    } else {
+        perror("Error listening for connections");
+        exit(EXIT_FAILURE);
+    }
+
+    addr_size = sizeof(new_addr);
+
+    while (1) {
+        // Accept a new connection and create a thread for it
+        new_socket = accept(server_socket, (struct sockaddr*)&new_addr, &addr_size);
+        if (new_socket < 0) {
+            perror("Error accepting connection");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Connection accepted from %s:%d\n", inet_ntoa(new_addr.sin_addr), ntohs(new_addr.sin_port));
+
+        // Create a thread to handle the client
+        if (pthread_create(&tid[clients_count], NULL, handle_client, &new_socket) != 0) {
+            perror("Error creating thread");
+            exit(EXIT_FAILURE);
+        }
+
+        clients_count++;
+
+        // If there are too many clients, close the new socket
+        if (clients_count >= MAX_CLIENTS) {
+            printf("Maximum number of clients reached. Closing connection...\n");
+            close(new_socket);
+        }
+    }
+
+    // Close the server socket (never reached in this loop)
+    close(server_socket);
+
+    return 0;
 }
 
